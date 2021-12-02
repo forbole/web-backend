461a593b8722dd324d7014393c96d2d88a4fe5e5
rachelhox <rachelhoyuenyi@gmail.com>
2021-12-02 16:01:24 +0800

    tsconfig and scripts

--- start.ts
+++ start.ts
@@ -2,7 +2,7 @@ const app = require('./index');
 const PORT = process.env.PORT || 3000;
 const url = process.env.PUBLIC_URL;
 
-app.listen(PORT, (err?: any) => {
+app.listen(PORT, (err: unknown) => {
     if (err) throw err;
     console.log(`> Ready on ${url}`);
     console.log(`> ENV:  ${process.env.NODE_ENV}`);
