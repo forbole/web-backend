b87e7c7eeea52e27154a01812ede7a158ce2f8b5
rachelhox <rachelhoyuenyi@gmail.com>
2021-12-02 17:12:08 +0800

    config

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,3 +1,3 @@
-const app = require('../app') // Link to your server file
+const app = require('../index') // Link to your server file
 const supertest = require('supertest')
 const request = supertest(app)
