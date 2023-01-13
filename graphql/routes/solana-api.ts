b125ce768a55c79ea934e9b99abed2410081f9cb
rachelhox <rachelhoyuenyi@gmail.com>
2023-01-13 12:05:01 +0000

    max_over_time: solana API

--- graphql/routes/solana-api.ts
+++ graphql/routes/solana-api.ts
@@ -2,6 +2,9 @@ import { RESTDataSource } from '@apollo/datasource-rest';
 // KeyValueCache is the type of Apollo server's default cache
 import type { KeyValueCache } from '@apollo/utils.keyvaluecache';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+
 export class SolanaAPI extends RESTDataSource {
     override baseURL = `http://${process.env.PROMETHEUS_URL}/prometheus/api/v1/`
 
@@ -10,19 +13,19 @@ export class SolanaAPI extends RESTDataSource {
     }
 
     async getSolanaUsers(): Promise<any> {
-        return this.get<any>(`query?query=solana_validator_delegators_count`)
+        return this.get<any>(`query?query=max_over_time(solana_validator_delegators_count[${process.env.MAX_OVER_TIME_DURATION}])`)
     }
 
     async getStakedSolana(): Promise<any> {
-        return this.get<any>(`query?query=solana_validator_staked`)
+        return this.get<any>(`query?query=max_over_time(solana_validator_staked[${process.env.MAX_OVER_TIME_DURATION}])`)
     }
 
     async getSolanaTVL(): Promise<any> {
-        return this.get<any>(`query?query=solana_validator_staked{} * on (denom) group_left token_price`)
+        return this.get<any>(`query?query=max_over_time(solana_validator_staked{}[${process.env.MAX_OVER_TIME_DURATION}]) * on (denom) group_left token_price`)
     }
 
     async getSolanaCommission(): Promise<any> {
-        return this.get<any>(`query?query=solana_validator_commission_rate`)
+        return this.get<any>(`query?query=max_over_time(solana_validator_commission_rate[${process.env.MAX_OVER_TIME_DURATION}])`)
     }
 
     async getSolanaUnbondingTime(): Promise<any> {
