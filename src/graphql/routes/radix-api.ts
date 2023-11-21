7bcf0badccdf92f53985f26b3eb1b8babde00fa9
Ignacio <ignacio@forbole.com>
2023-11-21 15:58:16 +0800

    refactor: radix api and env defaults

--- src/graphql/routes/radix-api.ts
+++ src/graphql/routes/radix-api.ts
@@ -1,19 +1,29 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 import type { KeyValueCache } from "@apollo/utils.keyvaluecache";
 
+const { RADIX_URL } = process.env;
+
+if (!RADIX_URL) {
+  throw new Error("Missing RADIX_URL env var");
+}
+
 export class RadixAPI extends RESTDataSource {
-  override baseURL = `${process.env.RADIX_URL}/`;
+  override baseURL = `${(RADIX_URL as string).replace(/\/$/, "")}/`;
 
   constructor(options: { cache: KeyValueCache }) {
     super(options); // this sends our server's `cache` through
   }
 
-  async getTotalRadixSupply(body: any) {
+  async getTotalRadixSupply() {
     return this.post("token/native", {
       headers: {
         "Content-Type": "application/json",
       },
-      body,
+      body: JSON.stringify({
+        network_identifier: {
+          network: "mainnet",
+        },
+      }),
     });
   }
 
