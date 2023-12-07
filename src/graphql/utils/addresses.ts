18b73c79b890c6c752673e349ff9be810ca5d8c3
Ignacio <142767109+icfor@users.noreply.github.com>
2023-12-07 21:02:34 +0800

    refactor: api addresses (#32)

---
+++ src/graphql/utils/addresses.ts
@@ -0,0 +1,9 @@
+export const archwayValidatorAddress =
+  "archwayvaloper1esg4kluvdkfcxl0atcf2us2p9m9y9sjjsu04ex";
+
+export const radixValidatorAddress =
+  "validator_rdx1swkmn6yvrqjzpaytvug5fp0gzfy9zdzq7j7nlxe8wgjpg76vdcma8p";
+
+// https://suiexplorer.com/validator/0x1e1985024aafe50a8e4eafc5a89eb7ecd58ba08c39f37688bee00bd55c8b2059
+export const suiValidatorAddress =
+  "0x1e1985024aafe50a8e4eafc5a89eb7ecd58ba08c39f37688bee00bd55c8b2059";
